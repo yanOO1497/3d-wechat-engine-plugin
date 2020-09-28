ff7afa6514b5704c99df4b004a25ca038dd73aa1
yanOO1497 <yuanyuan1497@foxmail.com>
2020-09-28 15:22:07 +0800

    update docs

--- packages/publish/hooks.js
+++ packages/publish/hooks.js
@@ -1,10 +1,17 @@
-import { moveSync, writeJSONSync } from 'fs-extra';
+import { removeSync, moveSync, writeJSONSync } from 'fs-extra';
 import { join } from 'path';
+import * as Editor from 'editor';
 
 export function onBeforeComplete(options, result) {
-    if (!options.separateEngine) {
+    const pkgOptions = options.packages.wechatgame;
+    if (!pkgOptions.separateEngine || !options.packages['wechatgame-Plugin'].publish) {
         return;
     }
+    // 将项目内除了 cocos 文件夹复制到 mini-game 文件夹内
+    const miniGameDir = join(__dirname, '../../cocosPlugin/minigame');
+    copySync(result.dest, miniGameDir);
+    removeSync(join(result.dest, 'cocos3d-js'));
+
     // 修改 game.json 内的 cocos 字段
     const gameJsonPath = join(result.paths.dir, 'game.json');
     const gameJson = readJsonSync(gameJsonPath);
@@ -13,6 +20,9 @@ export function onBeforeComplete(options, result) {
         provider: 'wx0446ba2621dda60a',
     };
     writeJSONSync(gameJsonPath, gameJson);
+
+    const dir = join(Editor.App.path, '../resources/3d/engine/bin/.cache/editor-cache/wechatgame/cocos');
     // 将打包出来的引擎移动到 plugin 内部
-    moveSync(result.paths.engine, join(__dirname, '../../cocosPlugin/plugin'));
+    moveSync(dir, join(__dirname, '../../cocosPlugin/plugin'));
+
 }
